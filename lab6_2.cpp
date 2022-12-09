83b9f8db1c615c21ed55d03b1ea3caec72cdf669
Anawat <janawat0123@gmail.com>
2022-12-09 16:47:58 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+double deg2rad(double degr){
+    return degr *(M_PI/180);
+}
+double rad2deg(double radi){
+   return radi*(180/M_PI);
+}
+
+double findXComponent(double L1,double L2,double radi1,double radi2){
+    return (L1*cos(radi1)) + (L2 * cos(radi2));
+}
+
+double findYComponent(double L1,double L2,double radi1,double radi2){
+    return (L1*sin(radi1)) + (L2 * sin(radi2));
+}
+
+double pythagoras(double x,double y){
+    return sqrt(pow(x,2)+pow(y,2));
+}
+
+void ShowResult(double ResultVecLength,double ResultVecDirection){
+    cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout<<"Length of the resultant vector = "<<ResultVecLength<<"\n";
+    cout<<"Direction of the resultant vector (deg) = "<<ResultVecDirection<<"\n";
+    cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
+
+
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
@@ -12,6 +40,8 @@ int main(){
     cin >> l2;
     cout << "Enter direction of the second vector (deg): ";
     cin >> a2;
+
+    
     
     a1 = deg2rad(a1);
     a2 = deg2rad(a2);
@@ -20,5 +50,7 @@ int main(){
     result_vec_length = pythagoras(xcomp,ycomp);
     result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
 
-    showResult(result_vec_length,result_vec_direction);
+    ShowResult(result_vec_length,result_vec_direction);
+
 }
+ 
